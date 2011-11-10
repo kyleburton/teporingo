3db60899e53814b5802bf2aa759c40447faf5b52
Super G <kburton@relaynetwork.com>
2011-11-10 17:38:43 -0500

    moved some common rpc code used by workflows into teporingo

---
+++ teporingo/src/teporingo/rpc.clj
@@ -0,0 +1,29 @@
+(ns teporingo.rpc
+  (:require
+   [clj-etl-utils.log                                 :as log]
+   [clojure.contrib.json                              :as json]
+   [teporingo.publish                                 :as mq-pub])
+  (:use
+   [teporingo.core           :only [publish]]
+   [clj-etl-utils.lang-utils :only [raise]]))
+
+
+(defn reply-to-message [message result & [other-data]]
+  ;; (mq-pub/lookup (keyword (get-in (first *x*) [:return-to :publisher])))
+  ;; (teporingo.broker/find-by-roles (:broker-roles (:sms-publisher @teporingo.publish/*publisher-registry*)))
+  (if (contains? message :return-to)
+    (let [return-payload (get-in message [:return-to])
+          publisher      (keyword (get-in message [:return-to :publisher]))
+          return-command (get-in message [:return-to :return-command])
+          reply-message  (assoc return-payload
+                           :result    result
+                           :exception (when (and other-data (contains? other-data :exception))
+                                        (:exception other-data)))
+          reply-message  (json/json-str {:command return-command :payload reply-message})]
+
+      (mq-pub/with-publisher publisher
+        (log/infof "replying with result: msg:%s res:%s publisher:%s" message result publisher)
+        (publish reply-message)))
+    (log/infof "No return-to for message: %s, discarding %s" message result)))
+
+
