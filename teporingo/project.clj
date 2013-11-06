e892d3ddbdb33e885937fa7da94440b9dd5ffbe3
Kyle Burton <kyle.burton@gmail.com>
2013-11-06 10:57:47 -0500

    lein-release plugin: bumped version from 2.1.22 to 2.1.23-SNAPSHOT for next development cycle

--- teporingo/project.clj
+++ teporingo/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.kyleburton/teporingo "2.1.22"
+(defproject com.github.kyleburton/teporingo "2.1.23-SNAPSHOT"
   :description "Teporingo: Rabbit Client Library"
   :url         "http://github.com/kyleburton/teporingo"
   :lein-release {:deploy-via :clojars :scm :git}
