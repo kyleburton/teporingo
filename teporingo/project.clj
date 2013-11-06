6f884d90a510924d0b4cb69530e09fc0f2f62e8e
Kyle Burton <kyle.burton@gmail.com>
2013-11-06 10:57:42 -0500

    lein-release plugin: preparing 2.1.22 release

--- teporingo/project.clj
+++ teporingo/project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.kyleburton/teporingo "2.1.22-SNAPSHOT"
+(defproject com.github.kyleburton/teporingo "2.1.22"
   :description "Teporingo: Rabbit Client Library"
   :url         "http://github.com/kyleburton/teporingo"
   :lein-release {:deploy-via :clojars :scm :git}
